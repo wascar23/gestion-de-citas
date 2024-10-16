1d60bae564e17acf97a766c5c8f66c4ab743e3ad
Adrian Ernesto Cuello Almonte <20230610@itla.edu.do>
2024-10-15 22:40:25 -0400

    Third Commit

--- CitasApp/CitasApp.Data/obj/Debug/net8.0/CitasApp.Data.AssemblyInfo.cs
+++ CitasApp/CitasApp.Data/obj/Debug/net8.0/CitasApp.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CitasApp.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70be4eccb5a007547d8105738c827d38755b55af")]
 [assembly: System.Reflection.AssemblyProductAttribute("CitasApp.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CitasApp.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
